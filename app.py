2cddbd48856ca4129e57a845831ddfb7af8467c4
ZiadAbdElSalam2003 <168100027+ZiadAbdElSalam2003@users.noreply.github.com>
2024-04-25 15:23:59 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+import nltk
+from nltk.corpus import stopwords
+nltk.download("stopwords")
+import gensim
+from gensim.utils import simple_preprocess
+file1 = open('C:\\Users\\gate\\OneDrive\\Desktop\\random_paragraphs.txt',"r")
+print(file1.readable())
+content=file1.read()
+english_stopwords=stopwords.words("english")
+english_stopwords
+text=[]
+for word in gensim.utils.simple_preprocess(content):
+    if word not in english_stopwords:
+        text.append(word) 
+file2 = open('C:\\Users\\gate\\OneDrive\\Desktop\\yes.txt',"w")
+file2.write(" ".join(text))
+d=dict()
+for element in text:
+    if element in d:
+        d[element]= d[element]+1
+    else:
+        d[element]=1
+print(d) 
